e80dc66d982e46f3cc8ba570ccc6397cf270e4ae
JunghoShin <jungho1902@korea.ac.kr>
2025-08-05 19:46:06 +0900

    Add cleanup functions for serial listeners

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -8,8 +8,8 @@ declare global {
       connectSerial: (portName: string) => Promise<boolean>;
       disconnectSerial: () => Promise<boolean>;
       sendToSerial: (data: string) => void;
-      onSerialData: (callback: (data: string) => void) => void;
-      onSerialError: (callback: (error: string) => void) => void;
+      onSerialData: (callback: (data: string) => void) => () => void;
+      onSerialError: (callback: (error: string) => void) => () => void;
       zoomIn: () => void;
       zoomOut: () => void;
       zoomReset: () => void;
